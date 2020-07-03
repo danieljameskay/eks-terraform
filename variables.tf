b6d983dbc0514cea5151db374e654a5c675c2077
Daniel Kay <daniel.kay@jet2.com>
2020-07-03 21:59:17 +0100

    adding users

--- variables.tf
+++ variables.tf
@@ -1,19 +1,3 @@
-# variable "map_users" {
-#   description = "Additional IAM users to add to the aws-auth configmap."
-#   type = list(object({
-#     userarn  = string
-#     username = string
-#     group    = string
-#   }))
-#   default = [
-#     {
-#       userarn  = ""
-#       username = ""
-#       group   = ""
-#     }
-#   ]
-# }
-
 variable "map_users" {
     type = string
 }
