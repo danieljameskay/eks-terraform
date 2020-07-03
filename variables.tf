7c050dd3d8e80f5a54290c2b74dc797161cbc81c
Daniel Kay <daniel.kay@jet2.com>
2020-07-03 13:46:27 +0100

    adding users

--- variables.tf
+++ variables.tf
@@ -1,4 +1,14 @@
 variable "users" {
-  type    = map(string)
-  default = ["default"]
+  type = list(object({
+    group = string
+    user_arn = string
+    username = string
+  }))
+  default = [
+    {
+      group = ""
+      user_arn = ""
+      username = ""
+    }
+  ]
 }
