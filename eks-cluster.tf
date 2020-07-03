58fff61939c04e67c48d713cbe2ab377c87aa29e
Daniel Kay <daniel.kay@jet2.com>
2020-07-03 14:27:20 +0100

    adding users

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,3 +1,11 @@
+data "external" "users" {
+  program = [${var.users}]
+}
+
+output "users_map" {
+  value = "${data.external.users.result}"
+}
+
 module "eks" {
   source       = "terraform-aws-modules/eks/aws"
   cluster_name = local.cluster_name
@@ -9,7 +17,7 @@ module "eks" {
     GithubOrg   = "terraform-aws-modules"
   }
 
-  map_users = variables.users
+  map_users = users_map
 
   vpc_id = module.vpc.vpc_id
 
