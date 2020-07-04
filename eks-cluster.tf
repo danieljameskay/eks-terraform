c4481886a8a857a56b44d87279b8d2ea4fa90c97
Daniel Kay <daniel.kay@jet2.com>
2020-07-04 21:59:37 +0100

    Tidy up after getting all the things to work :)

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,3 +1,11 @@
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
@@ -37,12 +45,4 @@ module "eks" {
       asg_desired_capacity          = 1
     },
   ]
-}
-
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
 }
