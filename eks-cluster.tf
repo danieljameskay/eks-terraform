3792c086d34ecb13af34d948b46688a74b3d6591
Daniel Kay <daniel.kay@jet2.com>
2020-07-04 21:44:26 +0100

    updated eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,3 +1,19 @@
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.9"
+}
+
 module "eks" {
   source       = "terraform-aws-modules/eks/aws"
   cluster_name = local.cluster_name
@@ -9,7 +25,7 @@ module "eks" {
     GithubOrg   = "terraform-aws-modules"
   }
 
-#   map_users = var.users
+  map_users = var.users
 
   vpc_id = module.vpc.vpc_id
 
