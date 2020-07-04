3b475c58b4f43d3818669d0e1436b4fd59518fcd
Danny Kay <danieljameskay@hotmail.co.uk>
2020-07-04 09:09:26 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "users" {
   description = "Additional IAM users to add to the aws-auth configmap."
   type = list(object({
-    userarn  = string
     username = string
+    userarn  = string
     groups   = list(string)
   }))
   default = [
     {
-      userarn  = ""
       username = ""
+      userarn  = ""
       groups   = [""]
     }
   ]
